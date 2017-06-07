4200293e7e3fec9090932df28a86cfc043c29128
vpecanins <vpecanins@users.noreply.github.com>
2017-06-07 19:46:25 +0200

    Remove swap

--- sincos/asm_sincos.s
+++ sincos/asm_sincos.s
@@ -20,18 +20,15 @@ asm_sincos:
     vpush {s4, s5}              @ We need to touch s4 and s5
     vmov.f32 r0, s0             @ Save s0 to r0
     and r0, #0x80000000         @ Keep only the sign of s0
-    vabs.f32 s0, s0             @ s0 = abs(x)
+    vabs.f32 s1, s0             @ s1 = abs(x)
     vldr s2, =0x3fc90fdb        @ s2 = pi/2
     vldr s3, =0x40490fdb        @ s3 = pi
-    vcmp.f32 s0, s2             @ if (abs(x) > pi/2)
+    vcmp.f32 s1, s2             @ if (abs(x) > pi/2)
     vmrs apsr_nzcv, fpscr       @ {
-    itt gt                      @   s0 = pi - x
-    vsubgt.f32 s0, s3, s0       @   save quadrant bit in r0
+    itt gt                      @   s1 = pi - abs(x)
+    vsubgt.f32 s1, s3, s1       @   save quadrant bit in r0
     orrgt r0, #0x40000000       @ }
-    vsub.f32 s1, s2, s0         @ s1 = pi/2 - x (to calculate cosine from sine)
-    vmov.f32 s4, s0             @ {
-    vmov.f32 s0, s1             @   Swap s0 and s1
-    vmov.f32 s1, s4             @ }
+    vsub.f32 s0, s2, s1         @ s0 = pi/2 - x (to calculate cosine from sine)
     vldr s3, =0x3ce7989b        @ a4 = 0.02827101
     vldr s2, =0xbe4c585c        @ a3 = -0.19955582
     vfma.f32 s2, s3, s0         @ s2 <= a3 + a4 * x
@@ -46,10 +43,10 @@ asm_sincos:
     vfma.f32 s0, s2, s0         @ s0 <= x + x^2 * (a2 + x * (a3 + a4 * x) )
     vfma.f32 s1, s4, s1         @ s1 <= x + x^2 * (a2 + x * (a3 + a4 * x) )
     tst r0, #0x80000000         @ {
-    it ne                       @		Fix sign of sin according to input sign
+    it ne                       @   Fix sign of sin according to input sign
     vnegne.f32 s1, s1           @ }
     tst r0, #0x40000000         @ {
-    it ne                       @		Fix sign of cos according to quadrant
-    vnegne.f32 s0, s0           @	}
+    it ne                       @   Fix sign of cos according to quadrant
+    vnegne.f32 s0, s0           @ }
     vpop {s4, s5}               @ Restore s4 / s5 (ARM Procedure Call Std)
     bx  lr                      @ Return to C
